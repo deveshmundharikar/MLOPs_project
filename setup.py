a31ac61b01a9b22ef93752f57bc25eb0ed973277
deveshmundharikar <rooknighty@gmail.com>
2025-11-24 21:33:56 +0530

    file structure ready

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Rookie",
+    author_email="rooknighty@gmail.com",
+    packages=find_packages()
+)
+
+
